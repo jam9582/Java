1026ef8cdefcd113580dd0ac055da04c786671f7
jam9582 <jjam9582@naver.com>
2025-03-24 03:19:02 +0900

    task

--- src/main/java/application/MultiThread.java
+++ src/main/java/application/MultiThread.java
@@ -6,5 +6,53 @@ package application;
 결과물로 두 개의 스레드가 동시에 실행되는 것을 보여주는 스크린샷을 제출합니다.
  */
 
-public class MultiThread{
+public class MultiThread {
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(
+                () -> { /* 여기서 람다식을 쓴게 implemets Runnable을 쓴 것
+                자바에서는 기본적으로 다중상속이 불가능하지만, 이러면 다중상속 가능
+                이거 대신에 Thread 상속으로도 대치할 수 있지만, 이러면 덜 유연해져서 implements Runnable 이 실무에서 선호됨
+                */
+                    for (int i=0 ; i<5 ; i++){
+                        System.out.println("Thread 1 실행됨: " + i);
+                        try {
+                            Thread.sleep(500); // 이거 쓰면 cmd + enter로 exception 던져줘야함
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+        // thread1.setPriority(Thread.MAX_PRIORITY); 이러면 thread1이 thread2보다 우선순위로 실행됨
+        // setPriority(int 1~10) 으로 우선순위 부여 가능, 숫자 클수록 우선시됨
+
+
+        Thread thread2 = new Thread(
+                () -> {
+                    for (int i=0 ; i<5 ; i++){
+                        System.out.println("Thread 2 실행됨: " + i);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join(); // 해당 스레드가 종료될 때까지 현재 스레드를 멈추는 것, main 메서드가 thread1이 끝날때까지 기다리는 것
+            thread2.join(); // start 2개 다 하고 그 뒤에 join을 2개 하면, 운에 따라 console 실행이 누가 먼저 될지 결정됨
+            // 1.start; 1.join; 2.start; 2.join; 이러면 실행 순서 고정을 시킬 순 있지만 멀티 스레드가 아니게 됨
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("모든 스레드 종료됨");
+
+    }
 }
--- src/main/java/application/ReverseString.java
+++ src/main/java/application/ReverseString.java
@@ -7,21 +7,31 @@ package application;
 결과물로 사용자가 입력한 문자열을 뒤집고 대소문자를 변환하여 출력하는 스크린샷을 제출합니다.
  */
 
+import java.util.Scanner;
+
 public class ReverseString {
-    public static String reverseAndToggleCase(String input) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            if (Character.isUpperCase(c)) {
-                sb.insert(0, Character.toLowerCase(c));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("영어 대소문자로 문자열을 입력해주세요: ");
+        String  sentence = sc.nextLine();
+
+        String  reverse = new StringBuilder(sentence).reverse().toString();
+        // .toString() 안하면 stringbuilder 타입으로 반환됨,
+        // 근데 이거 안 쓰고 바로 sout(reverse) 해도 프린트 잘 되는데, 그 이유는 내부적으로 알아서 obj.toString() 을 호출하기 때문
+
+        StringBuilder swap = new StringBuilder();
+        for (char ch : reverse.toCharArray()) { // 향상된 for-each문
+            if (Character.isUpperCase(ch)) {
+                swap.append(Character.toLowerCase(ch));
+            } else if (Character.isLowerCase(ch)) {
+                swap.append(Character.toUpperCase(ch));
             } else {
-                sb.insert(0, Character.toUpperCase(c));
+                swap.append(ch);
             }
         }
-        return sb.toString();
-    }
+        System.out.println("순서를 뒤집고 대소문자를 변환한 문자열: " + swap);
 
-    public static void main(String[] args) {
-        String input = "HelloWorld";
-        System.out.println("변환된 문자열: " + reverseAndToggleCase(input));
+        sc.close();
     }
 }
